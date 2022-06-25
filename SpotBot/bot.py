bd72db25b6e43d0024049d9331fdc96f7d05b8da
Alireza Ahmand <mr.alireza.ahmand@gmail.com>
2022-06-25 21:01:36 +0430

    Revert "Fixed bugs"

--- SpotBot/bot.py
+++ SpotBot/bot.py
@@ -1,19 +1,18 @@
 import urllib.request
-from spotify import MusicDownloader
+from SpotBot.spotify import MusicDownloader
 from pyrogram.types.bots_and_keyboards.reply_keyboard_markup import ReplyKeyboardMarkup
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.types import ReplyKeyboardRemove
-from pyrogram import enums
 import pyromod.listen
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import spotify as spotify
+import SpotBot.spotify as spotify
 import os
-from config import Configs
+from SpotBot.config import Configs
 icbot = Client('SpotSaveBot', bot_token=Configs.API_KEY,
-            api_id=Configs.API_ID, api_hash=Configs.API_HASH)
+               api_id=Configs.API_ID, api_hash=Configs.API_HASH)
 
 MAIN_KEYS = ReplyKeyboardMarkup(
     [['Download as FLAC', 'Download as MP3']], resize_keyboard=True)
@@ -32,17 +31,14 @@ async def about(Client, msg: Message):
 @icbot.on_message(filters.private & filters.regex('Download as MP3'))
 async def mp3dl(Client, msg: Message):
     status = None
-    status2 = None
     try:
         member = await icbot.get_chat_member(Configs.CHANNEL, msg.from_user.id)
-        member2 = await icbot.get_chat_member('XVPNPRO', msg.from_user.id)
-        status = member.status
-        status2 = member2.status
-    except:
+        status = member.status.value
+    except UserNotParticipant:
         pass
-    if (not status in [enums.chat_member_status.ChatMemberStatus.MEMBER,enums.chat_member_status.ChatMemberStatus.OWNER, enums.chat_member_status.ChatMemberStatus.ADMINISTRATOR]) & (not status2 in [enums.chat_member_status.ChatMemberStatus.MEMBER,enums.chat_member_status.ChatMemberStatus.OWNER, enums.chat_member_status.ChatMemberStatus.ADMINISTRATOR]):
-        await msg.reply_text(f'You must be a member of our channel\n@{Configs.CHANNEL}\n@XVPNPRO',
-                            reply_markup=ReplyKeyboardRemove(True))
+    if not status in ['member', 'owner', 'administrator']:
+        await msg.reply_text(f'You must be a member of our channel\n@{Configs.CHANNEL}',
+                             reply_markup=ReplyKeyboardRemove(True))
     else:
         inputs = await Client.ask(chat_id=msg.from_user.id, text='Paste Spotify song url or enter search query\nExample: **Cool - Dua Lipa**\nAbort /cancel', reply_markup=ReplyKeyboardRemove(True))
         if not inputs.text == '/cancel':
@@ -58,22 +54,15 @@ async def mp3dl(Client, msg: Message):
 
 @icbot.on_message(filters.private & filters.regex('Download as FLAC'))
 async def flacdl(Client, msg: Message):
-    status2 = None
     status = None
     try:
         member = await icbot.get_chat_member(Configs.CHANNEL, msg.from_user.id)
-        member2 = await icbot.get_chat_member('XVPNPRO', msg.from_user.id)
-        status = member.status
-        status2 = member2.status
-    except:
+        status = member.status.value
+    except UserNotParticipant:
         pass
-    if not status in [enums.chat_member_status.ChatMemberStatus.MEMBER,
-    enums.chat_member_status.ChatMemberStatus.OWNER, 
-    enums.chat_member_status.ChatMemberStatus.ADMINISTRATOR] and not status2 in [enums.chat_member_status.ChatMemberStatus.MEMBER,
-    enums.chat_member_status.ChatMemberStatus.OWNER, 
-    enums.chat_member_status.ChatMemberStatus.ADMINISTRATOR]:
-        await msg.reply_text(f'You must be a member of our channel\n@{Configs.CHANNEL}\n@XVPNPRO',
-                            reply_markup=ReplyKeyboardRemove(True))
+    if not status in ['member', 'owner', 'administrator']:
+        await msg.reply_text(f'You must be a member of our channel\n@{Configs.CHANNEL}',
+                             reply_markup=ReplyKeyboardRemove(True))
     else:
         inputs = await Client.ask(chat_id=msg.from_user.id, text='FLAC Downloader\nPaste Spotify song url or enter search query\nExample: **Cool - Dua Lipa**\nAbort /cancel', reply_markup=ReplyKeyboardRemove(True))
         if not inputs.text == '/cancel':
