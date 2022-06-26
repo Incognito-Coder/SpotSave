e02ca0d3bcc4d2f740c246fe7dc8486d0fc838b3
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2022-06-26 06:20:29 +0430

    album downloader added

--- SpotBot/spotify.py
+++ SpotBot/spotify.py
@@ -1,10 +1,15 @@
+import zipfile
 import spotdl.search.song_gatherer as Gather
 from spotdl.download.downloader import DownloadManager
 from spotdl.search import SpotifyClient
 import re
+import logging
 import os
 import subprocess
+
 from SpotBot.config import Configs
+
+logging.basicConfig(level=logging.INFO)
 
 SpotifyClient.init(client_id=Configs.CLIENT_ID,
                    client_secret=Configs.CLIENT_SECRET, user_auth=False)
@@ -31,9 +36,12 @@ async def MusicDownloader(query: str = None, format: str = None):
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     if(re.match(regex, query)):
         song = Gather.from_spotify_url(
-            spotify_url=query, output_format=format, use_youtube=True)
+            spotify_url=query, output_format=format)
         Sname = song.song_name
-        Sartist = song.album_artists
+        if len(song.album_artists) == 1:
+            Sartist = song.album_artists[0]
+        else:
+            Sartist = ' '.join(song.album_artists)
         Filename = song.file_name+'.' + format
         IFilename = song.file_name+'.mp3'
         Scaption = song.lyrics
@@ -50,9 +58,12 @@ async def MusicDownloader(query: str = None, format: str = None):
                         ["ffmpeg", "-i", song.file_name+'.mp3', Filename, "-map", "1", "-map_metadata", "0", "-c", "copy", "-movflags", "use_metadata_tags"], stdout=subprocess.PIPE).stdout.read()
     else:
         song = Gather.from_search_term(
-            query=query, output_format=format, use_youtube=True)
+            query=query, output_format=format)
         Sname = song[0].song_name
-        Sname = song[0].album_artists
+        if len(song[0].album_artists) == 1:
+            Sartist = song[0].album_artists[0]
+        else:
+            Sartist = ','.join(song[0].album_artists)
         Filename = song[0].file_name+'.' + format
         IFilename = song[0].file_name+'.mp3'
         Scaption = song[0].lyrics
@@ -67,6 +78,31 @@ async def MusicDownloader(query: str = None, format: str = None):
                 else:
                     subprocess.Popen(
                         ["ffmpeg", "-i", song[0].file_name+'.mp3', Filename, "-map", "1", "-map_metadata", "0", "-c", "copy", "-movflags", "use_metadata_tags"], stdout=subprocess.PIPE).stdout.read()
+
+
+async def AlbumDownloader(url: str = None):
+    """
+    url: give spotify album link here
+    """
+    global Sartist
+    global Sname
+    global Scover
+    songs = Gather.from_album(album_url=url)
+    for o in songs:
+        Sname = o.album_name
+        Scover = o.album_cover_url
+        if len(o.album_artists) == 1:
+            Sartist = o.album_artists[0]
+        else:
+            Sartist = ' '.join(o.album_artists)
+        with DownloadManager() as DM:
+            await DM.download_song(o)
+    archive = zipfile.ZipFile(f'{Sname}.zip', 'w', zipfile.ZIP_DEFLATED)
+    for file in songs:
+        archive.write(f'{file.file_name}.mp3', f'{file.file_name}.mp3')
+        os.remove(f'{file.file_name}.mp3')
+
+
 Sname = None
 Sartist = None
 Scaption = None
